f9c55a00867320b40c201b69b3fd98f53fee86f1
sol <daniel.ovchinnikov@gmail.com>
2017-04-28 22:19:13 +0300

    new rus dictionary

--- main.go
+++ main.go
@@ -175,7 +175,7 @@ func buildReply(game words, Text string) string {
 }
 
 func load() (map[rune][]string) {
-	file, err := ioutil.ReadFile("nouns.txt")
+	file, err := ioutil.ReadFile("/dict/words_rus")
 	if err != nil {
 		panic(err)
 	}
